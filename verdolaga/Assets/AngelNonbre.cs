1f9be455180a0877205a7dd85228cb6aef8db407
darkcart0945 <shadowx0945@gmail.com>
2025-11-29 17:07:48 -0400

    nombre push

--- verdolaga/Assets/AngelNonbre.cs
+++ verdolaga/Assets/AngelNonbre.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
-public class AngelNombre : MonoBehaviour
+public class AngelNonbre : MonoBehaviour
 {
+    public string Nombre = "Angel Luis";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
