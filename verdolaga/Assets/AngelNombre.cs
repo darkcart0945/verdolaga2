e58bc776dc8d7c3f1efde621a1be88b803cf69fd
Leandro Cruz <Leandromono23@gmail.com>
2025-11-29 22:10:13 -0400

    Agregué el método SaludoLeandro al script AngelNombre

--- verdolaga/Assets/AngelNombre.cs
+++ verdolaga/Assets/AngelNombre.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 public class AngelNombre : MonoBehaviour
 {
     public string Nombre = "Angel Luis";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Cambio realizado por Leandro
+    public void SaludoLeandro()
+    {
+        Debug.Log("Este método lo agregó Leandro.");
     }
 }
